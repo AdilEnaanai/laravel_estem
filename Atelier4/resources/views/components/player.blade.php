3216b668bef75eca914276a0dd0b017a832548fb
ENAANAI <aenaanai@uae.ac.ma>
2026-04-20 19:23:30 +0100

    problème réglé

--- Atelier4/resources/views/components/player.blade.php
+++ Atelier4/resources/views/components/player.blade.php
@@ -1,7 +1,7 @@
 @vite(['resources/css/app.css'])
 <div class="player-card">
     <h3>{{$name}}</h3>
-    <img src="{{$photoURL}}" alt="{{$name}}" style="width: 200px; height: auto;">
+    <img src="{{$photoURL}}" alt="{{$name}}">
     <p>
        {{$slot}} 
     </p>
--- Atelier4/resources/views/test.blade.php
+++ Atelier4/resources/views/test.blade.php
@@ -8,14 +8,14 @@
     <title>Document</title>
 </head>
 <body>
+    <div class="container">
 <x-player class="player" name="kylian Mbappe" photoURL="https://static.onzemondial.com/8/2026/04/photo_article/985907/401091/1200-L-ligue-des-champions-au-real-madrid-kylian-mbapp-appelle-une-remise-en-question-aprs-llimination-face-au-bayern.jpg">
 Kylian Mbappé, né le 20 décembre 1998 à Paris, est un footballeur international français qui évolue au poste d'attaquant au Real Madrid. Considéré comme l’un des meilleurs joueurs du monde, il est reconnu pour ses capacités de percussion et sa très grande vitesse. 
 </x-player>
 
 <x-player class="player" name="Achraf Hakimi" photoURL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8H_zEyj-QV6enY6KdLmnA-o9bq_JchjRfRg&s">
 Achraf Hakimi, né le 4 novembre 1998 à Madrid, est un footballeur international marocain jouant au poste d'arrière droit au Paris Saint-Germain. Formé au Real Madrid, il participe à l'UEFA Youth League avant de faire ses débuts en équipe première sous Zinédine Zidane, remportant la Ligue des champions 2017-2018
-és
 </x-player>
-
+    </div>
 </body>
 </html>
